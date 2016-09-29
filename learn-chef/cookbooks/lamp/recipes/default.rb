a93a68eb0390314d689185ad277b7cad2d0325f4
Anup Kumar <Anup Kumar>
2016-09-29 00:04:04 -0700

    Updating lamp default reciper and serverspec

--- learn-chef/cookbooks/lamp/recipes/default.rb
+++ learn-chef/cookbooks/lamp/recipes/default.rb
@@ -19,23 +19,24 @@ package ['nano','git','unzip','screen']
 package ['httpd','httpd-devel', 'httpd-devel', 'httpd-tools']
 
 #chkconfig --add httpd
-execute 'chkconfig --add httpd'
+#execute 'chkconfig --add httpd'
 #chkconfig httpd on
-execute 'chkconfig httpd on'
+#execute 'chkconfig httpd on'
 
 #service httpd stop
 service "httpd" do
-  action :stop
-end
+	supports :status => true, :restart => true, :reload => true
+  	action [ :enable, :stop ]
+ end
 
 #rm -rf /var/www/html
 
-execute "rm -rf /var/www/html" do
-  only_if { File.exist?("/var/www/html") }
-end
+# execute "rm -rf /var/www/html" do
+#   only_if { File.exist?("/var/www/html") }
+# end
 
 #ln -s /vagrant /var/www/html
-execute 'ln -s /vagrant /var/www/html'
+#execute 'ln -s /vagrant /var/www/html'
 
 #service httpd start
 service "httpd" do
@@ -44,35 +45,37 @@ end
 
 # PHP
 #yum install -y php php-cli php-common php-devel php-mysql
-package['php','php-cli','php-common','php-devel','php-mysql']
+package ['php','php-cli','php-common','php-devel','php-mysql']
 
 # MySQL
 #yum install -y mysql mysql-server mysql-devel
-package['mysql','mysql-server','mysql-devel']
+#package ['mysql-server']
+package ['mariadb-server','mariadb']
 
 
 #chkconfig --add mysqld
-execute 'chkconfig --add mysqld'
+#execute 'chkconfig --add mysqld'
 
 #chkconfig mysqld on
-execute 'chkconfig mysqld on'
+#execute 'chkconfig mysqld on'
 
 #service sqld start
-service "sqld" do
-  action :start
-end
+service "mariadb" do
+	supports :status => true, :restart => true, :reload => true
+ 	action [ :enable, :start ]
+ end
 
 #mysql -u root -e "SHOW DATABASES";
 execute 'mysql -u root -e "SHOW DATABASES";'
 
 # Download starter content
 #cd /vagrant; sudo -u vagrant wget -q https://raw.githubusercontent.com/knightz007/vagrant/master/files/index.html
-execute 'cd /vagrant; sudo -u vagrant wget -q https://raw.githubusercontent.com/knightz007/vagrant/master/files/index.html'
+execute 'cd /var/www/html; sudo wget -q https://raw.githubusercontent.com/knightz007/vagrant/master/files/index.html'
 
 #cd /vagrant;sudo -u vagrant wget -q https://raw.githubusercontent.com/knightz007/vagrant/master/files/info.php
-execute 'cd /vagrant;sudo -u vagrant wget -q https://raw.githubusercontent.com/knightz007/vagrant/master/files/info.php'
+execute 'cd /var/www/html; sudo wget -q https://raw.githubusercontent.com/knightz007/vagrant/master/files/info.php'
 
 #service httpd restart
-ervice "httpd" do
+service "httpd" do
   action :restart
 end
--- learn-chef/cookbooks/lamp/test/integration/default/serverspec/default_spec.rb
+++ learn-chef/cookbooks/lamp/test/integration/default/serverspec/default_spec.rb
@@ -8,19 +8,34 @@ describe package('httpd','httpd-devel', 'httpd-devel', 'httpd-tools') do
   it { should be_installed }
 end
 
-
-describe file('/var/www/html') do
-  it { should be_symlink }
+describe package('mariadb-server','mariadb') do
+  it { should be_installed }
 end
 
 
+
+packages = [ 'nano', 'git', 'unzip', 'screen', 'httpd', 'httpd-devel', 'httpd-tools', 'mariadb-server','mariadb' ]
+
+   packages.each do|p|
+      describe package(p) do
+        it { should be_installed }
+      end
+    end
+
+
 describe file('/var/www/html/info.php') do
-	it { should exist }
-	it { should be_directory }
+	it { should exist }	
+end
+
+describe file('/var/www/html/index.html') do
+	it { should exist }	
 end
 
 describe 'lamp::default' do
    describe command("curl http://localhost/info.php") do
    	its(:stdout) { should match /PHP/ }
+  end
+  describe command("curl http://localhost/index.html") do
+   	its(:stdout) { should match /lamp/ }
   end
 end
