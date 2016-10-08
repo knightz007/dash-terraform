f6aab85007382ee451b0f40e86fa9886e03cdd8c
Anup Kumar <Anup Kumar>
2016-10-08 00:33:57 -0700

    Updating default attr/recipes and adding recipe for web

--- learn-chef/cookbooks/awesome_customers_rhel/attributes/default.rb
+++ learn-chef/cookbooks/awesome_customers_rhel/attributes/default.rb
@@ -4,3 +4,4 @@ default['awesome_customers_rhel']['open_ports'] = 80
 
 default['awesome_customers_rhel']['user'] = 'web_admin'
 default['awesome_customers_rhel']['group'] = 'web_admin'
+default['awesome_customers_rhel']['document_root'] = '/var/www/customers/public_html'
