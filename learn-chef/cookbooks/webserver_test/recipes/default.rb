472d0c33cbeabbc6594e3cd94ec217521ddd012f
Anup Kumar <Anup Kumar>
2016-10-27 20:05:08 -0700

    Updating the default recipe and default spec recipe

--- learn-chef/cookbooks/webserver_test/recipes/default.rb
+++ learn-chef/cookbooks/webserver_test/recipes/default.rb
@@ -5,12 +5,22 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 # Install the Apache package.
-package 'httpd'
+package_name =
+  service_name =
+    case node['platform']
+    when 'centos' then 'httpd'
+    when 'ubuntu' then 'apache2'
+    end
 
-service 'httpd'  do
-	action [:enable, :start]
+# Install the package.
+package package_name
+
+# Start and enable the service.
+service service_name do
+  action [:enable, :start]
 end
 
+# Serve a custom home page.
 file '/var/www/html/index.html' do
   content '<html>
   <body>
